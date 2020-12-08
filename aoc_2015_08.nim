01f28e4d27028d7284464887ce52719db704ffe7
Duncan Clarke <bunkford@gmail.com>
2020-12-07 22:08:41 -0500

    Day 8 Part 2

--- aoc_2015_08.nim
+++ aoc_2015_08.nim
@@ -1,12 +1,16 @@
-import nre, strutils
+import nre, strutils, json
 
 proc input(file:string):seq[string] =
   for line in file.lines:
     result.add(line)
 
-proc solve1(input:seq[string]): int =
+proc solve1(input:seq[string]):int =
   for line in input:
-    var line_len = line.len
     inc(result, line.len - line.strip(chars = {'"'}).replace("\\\"", "\"").replace("\\\\", "\\").replace(re"\\x[a-fA-F0-9][a-fA-F0-9]", "@").len)
 
+proc solve2(input:seq[string]):int =
+  for line in input:
+      inc(result, line.escapeJSON.len - line.len)
+
 echo "Part 1 answer: ", solve1(input("./aoc_2015_08.txt")) 
+echo "Part 2 answer: ", solve2(input("./aoc_2015_08.txt")) # 1900 too low
