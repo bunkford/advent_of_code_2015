2f83e94ed43a1a527f471dce2cb42a20c67adfcd
Duncan Clarke <bunkford@gmail.com>
2020-12-10 15:35:03 -0500

    Part 1 & 2 cleaned up visualization

--- aoc_2015_14.nim
+++ aoc_2015_14.nim
@@ -1,7 +1,7 @@
 import strscans, strutils, tables, osproc
 
 
-proc solve1(): int =
+proc solve(): int =
   var table = initTable[(string, string), int]()
   var map = initTable[string, string]()
   var reindeers:seq[string]
@@ -14,6 +14,7 @@ proc solve1(): int =
       table[(reindeer, "duration")] = duration
       table[(reindeer, "rest")] = rest
 
+      table[(reindeer, "points")] = 0 # seconds flying
       table[(reindeer, "distance")] = 0 # distance traveled
 
       table[(reindeer, "stopped")] = 0 # seconds stopped
@@ -42,17 +43,32 @@ proc solve1(): int =
         table[(deer, "distance")] += table[(deer, "speed")]
         map[deer] = repeat(' ', table[(deer, "distance")] div 50) & "🦌"
 
-    discard execCmd "clear"
-    for k, v in map:
-      echo k, ":", v
+    var dp:seq[int]
+    for k, v in table:
+      if k[1] == "distance":
+        dp.add(v)
+    for k, v in table:
+      if k[1] == "distance":
+        if v == dp.max: 
+          table[(k[0], "points")] += 1
 
-  var winner:string
+    discard execCmd "clear"
+    echo alignLeft("Name", 8) & alignLeft("Pts.", 5) & alignLeft("Dist.", 8)
+    for k, v in map:
+      echo alignLeft(k, 8) & alignLeft($table[(k, "points")], 5) & alignLeft($table[(k, "distance")] & " Km", 7) & "|" & alignLeft(v, 56) & "|"
+  var distance_winner, points_winner:string
+  var points:int 
   for k, v in table:
     if k[1] == "distance":
       if v > result: 
         result = v
-        winner = k[0]
-  echo "Winner: ", winner
+        distance_winner = k[0]
+    if k[1] == "points":
+      if v > points:
+        points = v
+        points_winner = k[0]
+  echo "Distance Winner: ", distance_winner , " @ ", result, " Km"
+  echo "Points Winner: ", points_winner , " @ ", points 
     
-echo "Part 1 answer: ", solve1() # 3312 too high
+discard solve()
      
